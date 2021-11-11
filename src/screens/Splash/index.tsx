f5a98a2ec4fc0485d482e74ef7ba2da147183845
Oleksii Ilchenko <oilche@softserveinc.com>
2021-11-11 12:01:54 +0200

    add theming, refactor

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -2,17 +2,20 @@ import React, { FC, useEffect } from 'react';
 import { SafeAreaView, Text } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { getSplashToken } from '@screens/Login/redux/actionCreators';
+import { useTheme } from '@react-navigation/native';
+import { styles } from './styles';
 
 export const Splash: FC = () => {
   const dispatch = useDispatch();
+  const { colors } = useTheme();
 
   useEffect(() => {
     dispatch(getSplashToken());
   }, [dispatch]);
 
   return (
-    <SafeAreaView>
-      <Text>CUBE GAME</Text>
+    <SafeAreaView style={{ ...styles.container, backgroundColor: colors.background }}>
+      <Text style={{ ...styles.text, color: colors.text }}>CUBE GAME</Text>
     </SafeAreaView>
   );
 };
